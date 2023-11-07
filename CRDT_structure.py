c7b28a8b2d7ac62ac59857ee953ef7b793f427ee
DuninD <dunin-04@mail.ru>
2023-11-07 13:04:41 +0500

    fix merge 2

--- CRDT_structure.py
+++ CRDT_structure.py
@@ -62,47 +62,33 @@ class CRDTnew:
 
 class Merge:
     def __init__(self):
-        self.set_merge = set()
         pass
     def merge(self, crdt1, crdt2):
         first_index = 0
         second_index = 0
         crdt = CRDTnew("")
+        crdt_values = []
         while first_index < len(crdt1.blocks) and second_index < len(crdt2.blocks):
             if crdt1.blocks[first_index][1] < crdt2.blocks[second_index][1]:
-                if crdt1.blocks[first_index][0] == crdt2.blocks[second_index][0] and \
-                        (''.join(crdt1.blocks[first_index][0]), crdt1.blocks[first_index][1], crdt1.blocks[first_index][2]) in self.set_merge:
+                if crdt1.blocks[first_index][0] not in crdt_values:
                     crdt.insert(len(crdt.blocks), crdt1.blocks[first_index][0])
-                    first_index += 1
-                    second_index += 1
-                else:
-                    crdt.insert(len(crdt.blocks), crdt1.blocks[first_index][0])
-                    first_index += 1
-                save = crdt.blocks[-1]
-                self.set_merge.add((''.join(save[0]), save[1], save[2]))
-            elif crdt1.blocks[first_index][1] > crdt2.blocks[second_index][1]:
-                if crdt1.blocks[first_index][0] == crdt2.blocks[second_index][0] and\
-                    (''.join(crdt1.blocks[first_index][0]), crdt1.blocks[first_index][1]) in self.set_merge:
-                    crdt.insert(len(crdt.blocks), crdt1.blocks[first_index][0])
-                    first_index += 1
-                    second_index += 1
-                else:
-                    crdt.insert(len(crdt.blocks), crdt2.blocks[second_index][0])
-                    second_index += 1
-                save = crdt.blocks[-1]
-                self.set_merge.add((''.join(save[0]), save[1], save[2]))
-            elif crdt1.blocks[first_index][1] == crdt2.blocks[second_index][1]:
-                crdt.insert(len(crdt.blocks), crdt1.blocks[first_index][0])
+                    crdt_values.append(crdt1.blocks[first_index][0])
                 first_index += 1
+            else:
+                if crdt2.blocks[second_index][0] not in crdt_values:
+                    crdt_values.append(crdt2.blocks[second_index][0])
+                    crdt.insert(len(crdt.blocks), crdt2.blocks[second_index][0])
                 second_index += 1
-                save = crdt.blocks[-1]
-                self.set_merge.add((''.join(save[0]), save[1], save[2]))
         if first_index == len(crdt1.blocks):
             for i in range(second_index, len(crdt2.blocks)):
-                crdt.insert(len(crdt.blocks), crdt2.blocks[i][0])
+                if crdt2.blocks[second_index][0] not in crdt_values:
+                    crdt_values.append(crdt2.blocks[i][0])
+                    crdt.insert(len(crdt.blocks), crdt2.blocks[i][0])
         else:
             for i in range(first_index, len(crdt1.blocks)):
-                crdt.insert(len(crdt.blocks), crdt1.blocks[i][0])
+                if crdt1.blocks[first_index][0] not in crdt_values:
+                    crdt_values.append(crdt1.blocks[i][0])
+                    crdt.insert(len(crdt.blocks), crdt1.blocks[i][0])
         crdt1.blocks = copy.deepcopy(crdt.blocks)
         for block in crdt1.blocks:
             block[2] = crdt1.replica_id
