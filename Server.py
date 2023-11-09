a76ac567538505d3a5e10082b56bfcac0e27ad58
ninimimm <nikitachernov89221156246@yandex.ru>
2023-11-09 23:58:01 +0500

    encode-decode

--- Server.py
+++ Server.py
@@ -13,12 +13,13 @@ class Server:
         crdt = cv.convert_string_to_crdt(data)
         self.Merge.merge(crdt, self.crdt)
         print(self.crdt.blocks)
-        server.sendto(cv.convert_crdt_to_str(crdt), address)
+        server.sendto(cv.convert_crdt_to_str(crdt).encode("utf-8"), address)
 
 def handle_clients():
     while True:
         try:
             data, address = server.recvfrom(1024)
+            data = data.decode("utf-8")
             if len(data) > 0:
                 server_class.merge_and_send_crdt(address, converter, data)
         except (ConnectionResetError, OSError) as Ex:
